022375af670a2a3f24aa5ba1083817de45411271
kk-2005 <97383050+kk-2005@users.noreply.github.com>
2022-02-20 15:56:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,9 +26,9 @@ boy.addAnimation("JakeRunning",boyImg);
 leftBoundary=createSprite(0,0,100,800);
 
 // leftBoundary.invisible = false;
-leftBoundary.visible = true;
+// leftBoundary.visible = true;
 // leftBoundary.invisible = true;
-// leftBoundary.visible = false;
+leftBoundary.visible = false;
 
 
 rightBoundary=createSprite(410,0,100,800);
